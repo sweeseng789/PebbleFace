fba8dec1d9d5273b7c577a44bcc74dc98bc89a03
Setsuna <sweeseng123@gmail.com>
2015-09-25 21:44:55 +0800

    Testing 3

--- src/main.c
+++ src/main.c
@@ -1,26 +1,19 @@
 #include <pebble.h>
 
-Window *my_window;
-TextLayer *text_layer;
+static Window *s_main_window;
 
-void handle_init(void) {
-  my_window = window_create();
+static void init()
+{
 
-  text_layer = text_layer_create(GRect(0, 0, 144, 20));
-  window_stack_push(my_window, true);
 }
 
-void handle_deinit(void) {
-  text_layer_destroy(text_layer);
-  window_destroy(my_window);
-}
+static void deinit()
+{
 
-static void main_window_load(Window *window){
-  
 }
 
 int main(void) {
-  handle_init();
-  app_event_loop();
-  handle_deinit();
+	init();
+	app_event_loop();
+	deinit();
 }
